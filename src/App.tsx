8f45acfac0ac3c965ef669674fa9438529080a7e
Douglas Kreitmaier <douglasklopes@gmail.com>
2023-02-06 21:35:40 -0300

    Creates Routes and Pages Home and History

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { ThemeProvider } from 'styled-components'
-import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
 
+import { Router } from './Router'
+
+import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>OLA MUNDO</h1>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
